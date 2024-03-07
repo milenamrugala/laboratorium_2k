a1cd02cfa5ce7813030298bc87f5253c4b6c2494
milenamrugala <milena.mrugala@icloud.com>
2024-03-07 19:28:28 +0100

    Fraction Main

--- src/main/kotlin/MainFraction.kt
+++ src/main/kotlin/MainFraction.kt
@@ -2,16 +2,19 @@ fun main() {
     val firstNumber = Fraction(1,2)
     val secondNumber = Fraction(3,5)
 
-    println(firstNumber)
-    println(secondNumber)
+    println(firstNumber) // (1/2)
+    println(secondNumber) // (3/5)
 
     val sum = firstNumber.add(secondNumber) // (11/10)
     val sub = firstNumber.sub(secondNumber) // (-1/10)
     val mul = firstNumber.mul(secondNumber) // (3/10)
+    val inv = firstNumber.inv(secondNumber) // (5/3)
+    val div = firstNumber.div(secondNumber) // (5/6)
 
     println(sum)
     println(sub)
     println(mul)
-
+    println(inv)
+    println(div)
 
 }
